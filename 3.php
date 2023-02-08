5d0ffe23bb30d0b0671216dc998b2cf3783733e8
vcproo <hants666@163.com>
2023-02-08 21:03:37 +0800

    1

--- 3.php
+++ 3.php
@@ -1,5 +1,5 @@
 <?php
 use Core\Error;
 include 'Error.php';
-(new Error(false))->error();//false 不开启debug 日志保存；true 开启debug利于开发
+(new Error(false))->error();//false 不开启debug 日志保存；true 开启debug利于开发 
 require a;//警告错误
